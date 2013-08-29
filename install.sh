48b4c79615d5541fb04178c7c07b0b7d25d93311
spockwangs <wbbtiger@gmail.com>
2013-08-29 20:01:58 +0800

    Use copy instead of symlink when installing dot files in Windows.

--- install.sh
+++ install.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-for name in *; do
-    if [ "${name: -1}" == '~' ]; then
-        continue
-    fi
-    target="$HOME/.${name}"
-    if [ -e "$target" ]; then
-        if [ ! -L "$target" ]; then
-            echo "WARNING: \`$target' exists but not a symlink"
+OS=`uname -o`
+if [ "$OS" == "Cygwin" ]; then
+    for name in *; do
+        # Skip "*~" files.
+        if [ "${name: -1}" == '~' ]; then
             continue
-        else
-            unlink $target
         fi
-    fi
-    extension="${name##*.}"
-    if [ "$extension" != "md" -a "$extension" != "sh" ]; then
-        echo "Creating \`$target'"
-        ln -s "${PWD}/${name}" "$target"
-    fi
-done
+        target="$HOME/.${name}"
+        extension="${name##*.}"
+        if [ "$extension" != "md" -a "$extension" != "sh" ]; then
+            echo "Creating \`$target'"
+            rm -rf "$target"
+            cp -r "${PWD}/${name}" "$target"
+        fi
+    done
+else
+    for name in *; do
+        # Skip "*~" files.
+        if [ "${name: -1}" == '~' ]; then
+            continue
+        fi
+        target="$HOME/.${name}"
+        if [ -e "$target" ]; then
+            if [ ! -L "$target" ]; then
+                echo "WARNING: \`$target' exists but not a symlink"
+                continue
+            else
+                unlink $target
+            fi
+        fi
+        extension="${name##*.}"
+        if [ "$extension" != "md" -a "$extension" != "sh" ]; then
+            echo "Creating \`$target'"
+            ln -s "${PWD}/${name}" "$target"
+        fi
+    done
+fi
