498c2cf781f5cf7aca8a8e41e48d4f629d0b2207
spockwangs <wbbtiger@gmail.com>
2015-03-23 14:00:07 +0800

    Adjusted settings for emacs.

---
+++ emacs.d/lisp/init-package.el
@@ -0,0 +1,31 @@
+(require 'package)
+(package-initialize)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+;; Install default packages if necessary.
+(defvar default-packages
+  '(haskell-mode
+    color-theme
+    color-theme-solarized
+    markdown-mode
+    yasnippet)
+  "A list of default packages to be installed at launch.")
+
+(defun default-packages-installed-p ()
+  (loop for p in default-packages
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+(unless (default-packages-installed-p)
+  (message "%s" "Emacs is now refreshing its package database...")
+  (package-refresh-contents)
+  (message "%s" " done.")
+  ;; install the missing packages
+  (dolist (p default-packages)
+    (when (not (package-installed-p p))
+      (package-install p))))
+
+(provide 'init-package)
