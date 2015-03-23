bf24c1ae47df38ff00b3351d12a55c4052d0bec3
spockwangs <wbbtiger@gmail.com>
2015-03-23 21:50:32 +0800

    Added support for terminal.

--- emacs.d/lisp/init-theme.el
+++ emacs.d/lisp/init-theme.el
@@ -1,8 +1,9 @@
 ;; Set up color theme.
 (require 'color-theme)
-(eval-after-load "color-theme"
-  '(progn
-     (color-theme-initialize)
-     (color-theme-solarized-dark)))
+(if (display-graphic-p)
+    (eval-after-load "color-theme"
+      '(progn
+         (color-theme-initialize)
+         (color-theme-solarized-dark))))
 
 (provide 'init-theme)
