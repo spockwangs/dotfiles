a2fcf0966ea18d659a7e560fd043dda5e0cb1614
spockwangs <wbbtiger@gmail.com>
2015-03-23 14:00:07 +0800

    Adjusted settings for emacs.

---
+++ emacs.d/init.el
@@ -0,0 +1,512 @@
+;; -*- coding: utf-8-unix -*-
+;; Copyright (c) 2010-2014 wbb
+;;     All rights reserved.
+;;
+;; Emacs config file.
+;;
+;; Time-stamp: <2015-03-23 13:50:09 spockwang>
+;; 
+;; Contents
+;; --------
+;; * General
+;; * Look and feel
+;; * Enable default disabled key bindings
+;; * Global key bindings
+;; * Language Settings
+;; * Miscellaneous
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; General
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(require 'cl)
+(server-start)
+
+;; Set load-path variable.
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(require 'init-preload-local nil t)
+(require 'init-package)
+
+;; Set initial frame size.
+(setq initial-frame-alist '((top . 10) (left . 10) (width . 100) (height . 30)))
+
+;; Set up color theme.
+(require 'color-theme)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-initialize)
+     (color-theme-solarized-dark)))
+
+;; Load features and libraries.
+(require 'sams-lib)
+(require 'redo)
+(require 'pager)
+(require 'xcscope)
+(require 'yasnippet)
+(yas/global-mode 1)
+(load "base")
+(load "yic-buffer")
+(iswitchb-mode 1)
+(ido-mode 1)
+(require 'ace-isearch)
+(global-ace-isearch-mode +1)
+(setq ace-isearch-input-length 10)
+      
+;; Get a visual, instead of audio, feedback of an exception.
+(setq visible-bell nil)
+
+;; Enable `auto-insert-mode'.
+(auto-insert-mode)
+(custom-set-variables '(auto-insert-directory "~/.emacs.d/auto-insert/"))
+
+;; Associate yasnippet with auto-insert.
+;; Copied from http://www.emacswiki.org/emacs/AutoInsertMode.
+(defun my/autoinsert-yas-expand()
+  "Replace text in yasnippet template."
+  (yas/expand-snippet (buffer-string) (point-min) (point-max)))
+
+;; Do not show startup message.
+(setq inhibit-startup-message t)
+
+;; Set language environment and coding system.
+;; See `set-file-name-coding-system', `set-buffer-file-coding-system',
+;; `set-buffer-process-coding-system', `set-terminal-coding-system',
+;; `set-selection-coding-system' and `set-input-method'.
+(set-language-environment 'UTF-8)
+(prefer-coding-system 'gbk-dos)
+(prefer-coding-system 'utf-8-unix)
+
+;; Set the default directory when finding a file.
+(setq default-directory "~/")
+
+;; Set default major mode to text-mode.
+(setq default-major-mode 'text-mode)
+
+;; Set default fill column.
+(setq-default fill-column 100)
+(turn-on-auto-fill)
+
+;; Update time stamp string in the buffer before saving.
+(add-hook 'before-save-hook 'time-stamp)
+
+;; Save bookmark automatically.
+(setq bookmark-save-flag 1)
+
+;; Make buffer names unique.
+(require 'uniquify)
+(setq uniquify-buffer-name-style 'forward)
+
+;; Use `ibuffer' instead of `list-buffers'.
+(defalias 'list-buffers 'ibuffer)
+
+;; Split always vertically.
+(setq split-height-threshold 20)
+(setq split-width-threshold nil)
+
+(setq desktop-path '("~/.emacs.d/"))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Look and feel
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Set default frame font
+;; To check current font, run `M-x describe-font'.
+;(set-frame-font "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-iso10646-1")
+(set-face-font 'default "-outline-Monaco-normal-normal-normal-mono-18-*-*-*-c-*-iso10646-1")
+
+;; Set fonts for Chinese characters.
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+  (set-fontset-font
+   (frame-parameter nil 'font)
+   charset
+   (font-spec :family "华文楷体" :size 22)))
+
+;; Hide tool bar.
+(tool-bar-mode 0)
+
+;; Hide right scroll bar.
+(scroll-bar-mode 0)
+
+;; Hide the menu bar.
+(menu-bar-mode 0)
+
+;; Show line number at the mode line.
+(line-number-mode t)
+
+;; Show column number at the mode line.
+(column-number-mode t)
+
+;; Show line numbers in the left margin.
+(global-linum-mode)
+
+;; Display time and date at the mode line.
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+(display-time)
+
+;; Show system name and full path of buffer at the title bar.
+(setq frame-title-format
+      (list (format "%s %%S: %%j " (system-name))
+                    '(buffer-file-name "%f" (dired-directory
+                                             dired-directory "%b"))))
+
+;; Highlight current line.
+(global-hl-line-mode 1)
+
+;; Use space instead of tabs.
+(setq-default indent-tabs-mode nil)
+
+;; Set transient-mark-mode.
+(setq transient-mark-mode t)
+
+;; Syntax highlight.
+(global-font-lock-mode 1)
+
+;; Show matched parentheses.
+(show-paren-mode t)
+
+;; Automatic backup but no version control.
+(setq make-backup-files t)
+(setq version-control t)
+(setq delete-old-versions t
+      kept-new-versions 3
+      kept-old-versions 0)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable default disabled 
+;;   key bindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Enable 'C-x <' and 'C-x >'
+(put 'scroll-left 'disabled nil)
+(put 'scroll-right 'disabled nil)
+
+;; Enable "C-x n n" and "C-x n w".
+(put 'narrow-to-region 'disabled nil)
+
+;; Enable "C-x C-l".
+(put 'downcase-region 'disabled nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Global Key bindings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Shift a region left or right.
+(global-set-key (kbd "C-v") 'pager-page-down)
+(global-set-key (kbd "<next>") 'pager-page-down)
+(global-set-key (kbd "M-v") 'pager-page-up)
+(global-set-key (kbd "<prior>") 'pager-page-up)
+(global-set-key (kbd "M-<up>") 'pager-row-up)
+(global-set-key (kbd "M-<kp-8>") 'pager-row-up)
+(global-set-key (kbd "M-<down>") 'pager-row-down)
+(global-set-key (kbd "M-<kp-2>") 'pager-row-down)
+(global-set-key (kbd "C-<") 'base/shift-left)
+(global-set-key (kbd "C->") 'base/shift-right)
+(global-set-key (kbd "C-x y") 'base/copy-line)
+(global-set-key (kbd "C-x d") 'base/kill-current-word)
+
+;; Cycle through buffer list.  Require "yic-buffer.el".
+(global-set-key "\C-x\C-p" 'bury-buffer)
+(global-set-key "\C-x\C-n" 'yic-next-buffer)
+
+;; Set file local variable `coding' to utf-8-unix.
+(global-set-key
+ "\C-xc"
+ (lambda ()               
+   "Set file local variable `coding' to utf-8-unix at the first line."
+   (interactive)
+   (save-excursion (add-file-local-variable-prop-line 'coding 'utf-8-unix))))
+
+(global-set-key (kbd "M-q") 'sams-fill)
+
+(global-set-key (kbd "M-/") 'hippie-expand)
+(setq hippie-expand-try-functions-list 
+      '(yas/hippie-try-expand
+        try-expand-dabbrev
+        try-expand-dabbrev-visible
+        try-expand-dabbrev-all-buffers
+        try-expand-dabbrev-from-kill
+        try-complete-file-name-partially
+        try-complete-file-name
+        try-expand-all-abbrevs
+        try-expand-list
+        try-expand-line
+        try-complete-lisp-symbol-partially
+        try-complete-lisp-symbol))
+
+(global-set-key (kbd "C-x M-d") 'insert-current-date)
+(global-set-key (kbd "C-x M-t") 'insert-current-time)
+
+;; Bind function keys
+(define-key global-map (kbd "<f2>") 'set-mark-command)
+(define-key global-map (kbd "<f3>") 'revert-buffer)
+(define-key global-map (kbd "<f5>") 'undo)
+(define-key global-map (kbd "S-<f5>") 'redo)
+(define-key global-map (kbd "<f6>") 'sams-cm-rotate)
+(define-key global-map (kbd "S-<f6>") 'sams-cm-save-point)
+(define-key global-map (kbd "<f7>") 'ispell)
+(define-key global-map (kbd "<f8>") 'dired)
+(define-key global-map (kbd "<f9>") 'goto-line)
+(define-key global-map (kbd "S-<f9>") 'goto-char)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Language Settings
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(setq c-default-style
+      '((java-mode . "java")
+        (awk-mode . "awk")))
+
+;; Auto-load `jce-mode'.
+(autoload
+  'jce-mode
+  "jce-mode"
+  "Major mode for TAF specification language."
+  t)
+(add-to-list 'auto-mode-alist '("\\.jce\\'" . jce-mode))
+
+;; Use HTML mode for .wsp files.
+(add-to-list 'auto-mode-alist '("\\.wsp\\'" . html-mode))
+
+
+;;;;;;;;;;;;;;;;;;;;
+;; C/C++ mode
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'c-mode-common-hook
+ '(lambda ()
+    (subword-mode 1)
+    (turn-on-auto-fill)
+    (c-toggle-auto-newline -1)
+    (define-key c-mode-base-map [(return)] 'newline-and-indent)
+    (define-key c-mode-base-map (kbd "M-q") 'c-fill-paragraph)))
+
+(require 'spock-c-style)
+(add-hook 'c-mode-common-hook 'spock-set-c-style)
+
+;; Override the default setting to use C++-mode for .h files.
+(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+
+;; Define auto insert skeletons for C++ header and source files.
+(add-to-list
+ 'auto-insert-alist
+ '(("\\.\\([Hh]\\|hh\\|hpp\\)\\'" . "C/C++ header skeleton")
+   . ["template.h" c++-mode my/autoinsert-yas-expand]))
+(add-to-list
+ 'auto-insert-alist
+ '(("\\.\\(cc\\|cpp\\)\\'" . "C/C++ source skeleton")
+   . ["template.cc" c++-mode my/autoinsert-yas-expand]))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Java
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'java-mode-hook
+ '(lambda ()
+    (c-set-style "java")
+    (subword-mode 1)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; CSS
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'css-mode-hook
+ '(lambda ()
+    (turn-on-auto-fill)
+    (define-key css-mode-map [(return)] 'newline-and-indent)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Javascript
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'js-mode-hook
+ '(lambda ()
+    (subword-mode 1)
+    (turn-on-auto-fill)
+    (define-key js-mode-map [(return)] 'newline-and-indent)))
+
+(add-to-list
+ 'auto-insert-alist
+ '(("\\.\\(js\\)\\'" . "Javascript source skeleton")
+   . ["template.js" js-mode my/autoinsert-yas-expand]))
+
+;;;;;;;;;;;;;;;;;;;;
+;; AWK
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'awk-mode-hook
+ '(lambda ()
+    (c-set-style "awk")))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Verilog mode
+;;;;;;;;;;;;;;;;;;;;
+
+(defun my-verilog-mode-hook ()
+  (setq tab-width 4)
+  (setq verilog-indent-level 4)
+  (setq verilog-cexp-indent 4)
+  (setq verilog-case-indent 4)
+  (setq verilog-auto-indent-on-newline t))
+(add-hook 'verilog-mode-hook 'my-verilog-mode-hook)
+
+;;;;;;;;;;;;;;;;;;;;
+;; Haskell mode
+;;;;;;;;;;;;;;;;;;;;
+
+(autoload
+  'haskell-mode
+  "haskell-mode"
+  "Major mode for the Haskell programming language."
+  t)
+
+;; Use haskell-mode for .hs files.
+(add-to-list 'auto-mode-alist '("\\.hs\\'" . haskell-mode))
+
+(add-hook 
+ 'haskell-mode-hook
+ '(lambda ()
+    (turn-on-haskell-indent)
+    (turn-on-haskell-doc-mode)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Text mode
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'text-mode-hook
+ '(lambda ()
+    (subword-mode 1)
+    (turn-on-auto-fill)
+    (setq tab-width 4)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Emacs Lisp
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'emacs-lisp-mode-hook
+ '(lambda ()
+    (turn-on-auto-fill)
+    (define-key emacs-lisp-mode-map (kbd "C-c C-c") 'comment-region)
+    (define-key emacs-lisp-mode-map [(return)] 'newline-and-indent)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Perl
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'perl-mode-hook
+ '(lambda ()
+    (define-key perl-mode-map [(return)] 'newline-and-indent)))
+
+;; Define auto-insert skeletons for perl source code.
+(add-to-list
+ 'auto-insert-alist
+ '(("\\.\\(pl\\)\\'" . "Perl source skeleton")
+   . ["template.pl" perl-mode my/autoinsert-yas-expand]))
+
+;;;;;;;;;;;;;;;;;;;;
+;; XML and HTML
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'html-mode-hook
+ '(lambda ()
+    (turn-off-auto-fill)
+    (define-key html-mode-map [(return)] 'newline-and-indent)))
+
+(add-hook
+ 'xml-mode-hook
+ '(lambda ()
+    (turn-off-auto-fill)
+    (define-key xml-mode-map [(return)] 'newline-and-indent)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; SQL
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'sql-mode-hook
+ '(lambda ()
+    (turn-on-auto-fill)))
+
+;;;;;;;;;;;;;;;;;;;;
+;; Calendar
+;;;;;;;;;;;;;;;;;;;;
+
+(setq calendar-week-start 1)
+
+;;;;;;;;;;;;;;;;;;;;
+;; Shell script mode
+;;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'sh-mode-hook
+ '(lambda ()
+    (define-key sh-mode-map [(return)] 'newline-and-indent)))
+
+;;;;;;;;;;;;;;;;;;;
+;; EShell mode
+;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'eshell-mode-hook
+ '(lambda ()
+    (setq comint-prompt-regexp "^[^#$%>\n]*[#$] ")))
+
+;;;;;;;;;;;;;;;;;;;
+;; Shell mode
+;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+ 'shell-mode-hook
+ '(lambda ()
+    (setq comint-use-prompt-regexp 1)
+    (setq comint-prompt-regexp "^[^#$%>\n]*[#$] ")))
+
+;;;;;;;;;;;;;;;;;;;
+;; Php mode
+;;;;;;;;;;;;;;;;;;;
+
+(autoload
+  'php-mode
+  "php-mode"
+  "Major mode for PHP."
+  t)
+(add-to-list 'auto-mode-alist '("\\.php\\'" . php-mode))
+
+;;;;;;;;;;;;;;;;;;;
+;; Python mode
+;;;;;;;;;;;;;;;;;;;
+
+(add-hook
+  'python-mode-hook
+  '(lambda ()
+     (subword-mode 1)
+     (define-key python-mode-map [(return)] 'newline-and-indent)))
+(add-to-list
+ 'auto-insert-alist
+ '(("\\.py\\'" . "Python skeleton")
+   . ["template.py" python-mode my/autoinsert-yas-expand]))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Miscellaneous
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Auto save a session.
+(desktop-save-mode 1)
+(setq desktop-save t
+      desktop-load-locked-desktop t)
+
+;; Load local customizations.
+(require 'init-local nil t)
---
+++ emacs.d/lisp/base.el
@@ -0,0 +1,67 @@
+;; Copyright 2010. wbb
+;;     All rights reserved.
+;;
+;; base.el -- Common functions used in .emacs
+;;
+;; Time-stamp: <2014-08-07 14:23:34 spockwang>
+
+(defun base/shift-region (distance)
+  "Shift the selected region right if distance is postive, left if
+negative"
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      ;; Tell the command loop not to deactivate the mark
+      ;; for transient mark mode
+      (setq deactivate-mark nil))))
+
+(defun base/shift-left ()
+  "Shift the region left by tab-width."
+  (interactive)
+  (base/shift-region (- tab-width)))
+
+(defun base/shift-right ()
+  "Shift the region right by tab-width."
+  (interactive)
+  (base/shift-region tab-width))
+
+(defun base/copy-line (&optional arg)
+  "Copy current line to kill-ring without marking the line."
+  (interactive "P")
+  (let ((beg (line-beginning-position))
+        (end (line-end-position arg)))
+    (copy-region-as-kill beg end))
+  (message "Copied a line."))
+
+(defun base/emacs-maximize ()
+ "Maximize Emacs."
+ (interactive)
+ (if (win32-p)                    ; in Windows
+     ;; send maximize message to the window   
+     (w32-send-sys-command #xf030)
+   (if (emacs-p)                        ; in Linux
+       (progn
+        (x-send-client-message
+         nil 0 nil "_NET_WM_STATE" 32    
+         '(1 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+        (x-send-client-message
+         nil 0 nil "_NET_WM_STATE" 32
+         '(1 "_NET_WM_STATE_MAXIMIZED_VERT" 0)))
+     nil)))
+
+(defun base/insert-current-date ()
+  "Insert current date at current point."
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d")))
+
+(defun base/insert-current-time ()
+  "Insert current time at current point."
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d %H:%M:%S")))
+
+(defun base/kill-current-word ()
+  "Kill current word."
+  (interactive)
+  (backward-word)
+  (kill-word 1))
