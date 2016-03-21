270034cd564ce1c766f7cb12be77e8fa0f4907db
spockwangs <wbbtiger@gmail.com>
2016-03-21 15:04:51 +0800

    Changed install.bat.

--- install.bat
+++ install.bat
@@ -7,7 +7,8 @@ rd /S /Q %HOME%\.emacs.d
 mklink /d %HOME%\.emacs.d %DOTFILES_DIR%\emacs.d
 
 rem Install config files.
-for %%G in (bashrc, gitignore, vimrc, gitconfig, screenrc) DO (
+for %%G in (bashrc, dir_colors, fonts.conf, gitignore, indent.pro, vimrc,
+            gitconfig, screenrc, inputrc) DO (
     del /S /Q %HOME%\.%%G
     mklink %HOME%\.%%G %DOTFILES_DIR%\%%G
 )
