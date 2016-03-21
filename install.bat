c44a70b243373e5fe1a7544c310b5c27c882ed20
spockwangs <wbbtiger@gmail.com>
2016-03-21 14:56:27 +0800

    Added install scripts for Windows.

---
+++ install.bat
@@ -0,0 +1,13 @@
+rem Install scripts for Windows.
+
+set DOTFILES_DIR=%~dp0%
+
+rem Install config directory.
+rd /S /Q %HOME%\.emacs.d
+mklink /d %HOME%\.emacs.d %DOTFILES_DIR%\emacs.d
+
+rem Install config files.
+for %%G in (bashrc, gitignore, vimrc, gitconfig, screenrc) DO (
+    del /S /Q %HOME%\.%%G
+    mklink %HOME%\.%%G %DOTFILES_DIR%\%%G
+)
