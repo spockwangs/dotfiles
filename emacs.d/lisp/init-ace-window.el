168eaa0faafff6ec92192ad904b3892aa373ff5d
spockwangs <wbbtiger@gmail.com>
2016-03-23 09:57:21 +0800

    Added config for ace-windows.

---
+++ emacs.d/lisp/init-ace-window.el
@@ -0,0 +1,9 @@
+;;; init-ace-window.el ---                           -*- lexical-binding: t; -*-
+
+(require-package 'ace-window)
+(require 'ace-window)
+
+(global-set-key (kbd "M-p") 'ace-window)
+
+(provide 'init-ace-window)
+;;; init-ace-window.el ends here
---
+++ emacs.d/lisp/init-auto-mark.el
@@ -0,0 +1,7 @@
+;; init-auto-mark.el
+
+(require 'auto-mark)
+
+(global-auto-mark-mode 1)
+
+(provide 'init-auto-mark)
