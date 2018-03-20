1e47a40460ee44ef2805e13e3b02b3df54532736
spockwangs <wbbtiger@gmail.com>
2018-03-20 14:56:29 +0800

    Add bash_profile

--- emacs.d/auto-insert/template.js
+++ emacs.d/auto-insert/template.js
@@ -1,4 +1,4 @@
-// Copyright (c) `(format-time-string "%Y")` wbb.
+// Copyright (c) `(format-time-string "%Y")` spockwang.
 //     All rights reserved.
 //
 // Author: wbbtiger@gmail.com
--- install.bat
+++ install.bat
@@ -8,5 +8,5 @@ xcopy %DOTFILES_DIR%\emacs.d %HOME%\.emacs.d /E /Y
 rem Install config files.
 for %%G in (bashrc, dir_colors, fonts.conf, gitignore, indent.pro, vimrc,
             gitconfig, screenrc, inputrc) DO (
-    xcopy %DOTFILES_DIR%\%%G %HOME%\.%%G /Y
+    xcopy %DOTFILES_DIR%\%%G %HOME%\.%%G
 )
