f824d0455210d6216d75dfb442078a3cef0d1bc4
spockwangs <wbbtiger@gmail.com>
2018-04-11 14:55:50 +0800

    fix

--- emacs.d/lisp/init-python.el
+++ emacs.d/lisp/init-python.el
@@ -1,9 +1,12 @@
+(require-package 'python-mode)
+(require 'python-mode)
+
 (add-hook
   'python-mode-hook
   '(lambda ()
      (subword-mode 1)
      (define-key python-mode-map [(return)] 'newline-and-indent)))
 
-(add-to-list 'auto-mode-alist '("^BUILD\\'" . python-mode))
+(add-to-list 'auto-mode-alist '("[/]BUILD\\'" . python-mode))
 
 (provide 'init-python)
