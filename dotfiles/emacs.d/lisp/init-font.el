1a0b3793fa9724d55b4be8ea553f901ca2bafadd
spockwang(王兵兵) <spockwang@spockwang-PC3.tencent.com>
2022-06-15 15:57:26 +0800

    fix

--- dotfiles/emacs.d/lisp/init-font.el
+++ dotfiles/emacs.d/lisp/init-font.el
@@ -1,7 +1,8 @@
 ;; Set default frame font
 ;; To check current font, run `M-x describe-font'.
 ;(set-frame-font "-outline-Consolas-normal-normal-normal-mono-18-*-*-*-c-*-iso10646-1")
-(set-face-font 'default "-outline-Monaco-normal-normal-normal-mono-18-*-*-*-c-*-iso10646-1")
+;(set-face-font 'default "-outline-Monaco-normal-normal-normal-mono-18-*-*-*-c-*-iso10646-1")
+(set-frame-font "Monaco 14" nil t)
 
 ;; Set fonts for Chinese characters.
 (dolist (charset '(kana han symbol cjk-misc bopomofo))
