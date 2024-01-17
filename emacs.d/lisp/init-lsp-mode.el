39c2c463a717cb77f78edc3a7838eebd22692fa3
spockwang <spockwang@tencent.com>
2024-01-17 10:32:03 +0800

    fix

--- emacs.d/lisp/init-lsp-mode.el
+++ emacs.d/lisp/init-lsp-mode.el
@@ -37,3 +37,21 @@
   (setq lsp-enable-symbol-highlighting t
         lsp-ui-doc-enable t
         lsp-lens-enable t))
+
+(use-package company
+  ;; 等价于 (add-hook 'after-init-hook #'global-company-mode)
+  :hook (after-init . global-company-mode)
+  :config
+  ;; setq 可以像这样连着设置多个变量的值
+  (setq company-tooltip-align-annotations t ; 注释贴右侧对齐
+        company-tooltip-limit 20            ; 菜单里可选项数量
+        company-show-numbers t              ; 显示编号（然后可以用 M-数字 快速选定某一项）
+        company-idle-delay .2               ; 延时多少秒后弹出
+        company-minimum-prefix-length 1     ; 至少几个字符后开始补全
+        ))
+
+(use-package company
+  :hook (after-init . global-company-mode)
+  :config
+  (setq company-tooltip-limit 20
+        company-show-numbers t
