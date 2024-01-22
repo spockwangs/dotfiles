cc609e9ff6882e8ab7c9cce7553c1a4f51b1e24f
spockwang <wbbtiger@gmail.com>
2024-01-22 23:10:53 +0800

    fix

--- install.sh
+++ install.sh
@@ -5,7 +5,10 @@
 set -e
 CWD=$(pwd)
 
-for name in (bash_profile, bashrc, bin, dir_colors, emacs.d, fonts.conf, gitconfig, gitignore, gnomerc, indent.pro, inputrc, screenrc); do
-    unlink "${HOME}/.${name}"
-    ln "${CWD}/$name" "${HOME}/.${name}"
+for name in bin; do
+    ln -f -s "${CWD}/${name}" "${HOME}/${name}"
+done
+
+for name in bash_profile bashrc dir_colors emacs.d fonts.conf gitconfig gitignore gnomerc indent.pro inputrc screenrc; do
+    ln -f -s "${CWD}/$name" "${HOME}/.${name}"
 done
