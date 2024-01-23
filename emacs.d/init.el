939ac5f4078e3e40f8466b3537e60deeee7dd160
spockwang <wbbtiger@gmail.com>
2024-01-23 23:31:11 +0800

    fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 2010-2015 wbb
 ;;     All rights reserved.
 ;;
-;; Time-stamp: <2024-01-10 15:46:04 spockwang>
+;; Time-stamp: <2024-01-23 23:06:10 spock>
 ;;
 
 (require 'server)
@@ -51,3 +51,17 @@
 (require 'init-protobuf-mode)
 (require 'init-vc)
 (require 'init-desktop)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(auto-insert-directory "~/.emacs.d/auto-insert/")
+ '(package-selected-packages
+   '(protobuf-mode web-mode smex xcscope php-mode go-mode clang-format treemacs-nerd-icons treemacs yasnippet fill-column-indicator ctrlf solarized-theme doom-modeline)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
