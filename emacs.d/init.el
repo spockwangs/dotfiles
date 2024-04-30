7d5e9d9ad97f9991b77f364b7587d2c6eadf870f
spockwang <spockwang@tencent.com>
2024-04-30 16:01:55 +0800

    fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 2010-2015 wbb
 ;;     All rights reserved.
 ;;
-;; Time-stamp: <2024-01-23 23:06:10 spock>
+;; Time-stamp: <2024-04-30 15:58:50 spockwang>
 ;;
 
 (require 'server)
@@ -51,6 +51,7 @@
 (require 'init-protobuf-mode)
 (require 'init-vc)
 (require 'init-desktop)
+(require 'init-epilog)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -58,7 +59,7 @@
  ;; If there is more than one, they won't work right.
  '(auto-insert-directory "~/.emacs.d/auto-insert/")
  '(package-selected-packages
-   '(protobuf-mode web-mode smex xcscope php-mode go-mode clang-format treemacs-nerd-icons treemacs yasnippet fill-column-indicator ctrlf solarized-theme doom-modeline)))
+   '(yaml-mode protobuf-mode web-mode smex xcscope php-mode go-mode clang-format treemacs-nerd-icons treemacs yasnippet fill-column-indicator ctrlf solarized-theme doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
