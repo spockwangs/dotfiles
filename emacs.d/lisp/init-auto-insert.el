226e07896feefd6b3d8c5a383fb7354c5e90f65b
spockwang <spockwang@tencent.com>
2024-07-02 13:32:49 +0800

    fix

--- emacs.d/lisp/init-auto-insert.el
+++ emacs.d/lisp/init-auto-insert.el
@@ -1,37 +1,24 @@
 ;; Enable `auto-insert-mode'.
-(auto-insert-mode)
-(custom-set-variables '(auto-insert-directory "~/.emacs.d/auto-insert/"))
+(use-package autoinsert
+  :after (yasnippet)
+  :init
+  (setq auto-insert-directory "~/.emacs.d/auto-insert/")
+  (setq auto-insert-query nil)
+  (auto-insert-mode)
+  :config
+  (define-auto-insert "\\.\\([Hh]\\|hh\\|hpp\\)\\'" ["template.h" c++-mode autoinsert-yas-expand])
+  (define-auto-insert "\\.\\(cc\\|cpp\\)\\'" ["template.cc" c++-mode autoinsert-yas-expand])
+  (define-auto-insert "\\.js\\'" ["template.js" js-mode autoinsert-yas-expand])
+  (define-auto-insert "\\.scala\\'" ["template.scala" scala-mode autoinsert-yas-expand])
+  (define-auto-insert "\\.py\\'" ["template.py" python-mode autoinsert-yas-expand]))
 
 (defun autoinsert-yas-expand()
   "Replace text in yasnippet template."
   (yas-expand-snippet (buffer-string) (point-min) (point-max)))
 
 (use-package yasnippet
-  :defer t
-  :config
-  ;; Define auto insert skeletons for C++ header and source files.
-  (add-to-list
-   'auto-insert-alist
-   '(("\\.\\([Hh]\\|hh\\|hpp\\)\\'" . "C/C++ header skeleton")
-     . ["template.h" c++-mode autoinsert-yas-expand]))
-  (add-to-list
-   'auto-insert-alist
-   '(("\\.\\(cc\\|cpp\\)\\'" . "C/C++ source skeleton")
-     . ["template.cc" c++-mode autoinsert-yas-expand]))
-
-  (add-to-list
-   'auto-insert-alist
-   '(("\\.js\\'" . "Javascript skeleton")
-     . ["template.js" js-mode autoinsert-yas-expand]))
-
-  (add-to-list
-   'auto-insert-alist
-   '(("\\.scala\\'" . "Scala skeleton")
-     . ["template.scala" scala-mode autoinsert-yas-expand]))
-
-  (add-to-list
-   'auto-insert-alist
-   '(("\\.py\\'" . "Python skeleton")
-     . ["template.py" python-mode autoinsert-yas-expand])))
+  :ensure t
+  :init
+  (yas-global-mode 1))
 
 (provide 'init-auto-insert)
