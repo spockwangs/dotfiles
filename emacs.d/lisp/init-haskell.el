1abc1eb497c9c0b7686bddba6dfccf4c271a518c
spockwang <spockwang@tencent.com>
2024-08-16 10:15:39 +0800

    fix

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -1,6 +1,9 @@
 (use-package haskell-mode
+  :defer t
   :hook ((haskell-mode . turn-on-haskell-indent)
          (haskell-mode . turn-on-haskell-doc-mode))
-  :mode ("\\.hs\\'" . haskell-mode))
+  :mode ("\\.hsc?\\'" . haskell-mode)
+  :bind (:map haskell-mode-map
+              ("C-c h" . haskell-hoogle)))
 
 (provide 'init-haskell)
