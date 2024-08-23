2526fe42c4460517f1a277f2e166d7620dc4baad
spockwang <spockwang@tencent.com>
2024-08-23 20:20:31 +0800

    fix

--- emacs.d/lisp/init-sql.el
+++ emacs.d/lisp/init-sql.el
@@ -1,7 +1,13 @@
+(use-package reformatter)
+
 (use-package sql-mode
-  :ensure nil
+  :config
+  (require 'sqlformat)
+  (setq sqlformat-command 'sqlformat)
+  (setq sqlformat-args '("-k" "upper" "-r" "--indent_width" "4" "-s"))
   :hook (sql-mode . turn-on-auto-fill)
   :bind (:map sql-mode-map
-               ("C-c C-c" . comment-region)))
+              ("C-c C-c" . comment-region)
+              ("C-M-\\" . sqlformat)))
 
 (provide 'init-sql)
