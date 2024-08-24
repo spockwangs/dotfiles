7f19e84be240cef724b2afe658942ed8c4f51ca4
spockwang <wbbtiger@gmail.com>
2024-08-24 17:49:25 +0800

    fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 2010-2015 wbb
 ;;     All rights reserved.
 ;;
-;; Time-stamp: <2024-08-23 18:35:48 spockwang>
+;; Time-stamp: <2024-08-24 16:27:01 spock>
 ;;
 
 (require 'server)
@@ -59,7 +59,7 @@
  ;; If there is more than one, they won't work right.
  '(auto-insert-directory "~/.emacs.d/auto-insert/")
  '(package-selected-packages
-   '(reformatter haskell-mode yaml-mode protobuf-mode web-mode smex xcscope php-mode go-mode clang-format treemacs-nerd-icons treemacs yasnippet fill-column-indicator ctrlf solarized-theme doom-modeline)))
+   '(python-black reformatter haskell-mode yaml-mode protobuf-mode web-mode smex xcscope php-mode go-mode clang-format treemacs-nerd-icons treemacs yasnippet fill-column-indicator ctrlf solarized-theme doom-modeline)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
