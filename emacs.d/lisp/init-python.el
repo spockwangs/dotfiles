9c6926c86eaddd255dd5aec414b0396c213f8b21
spockwang <wbbtiger@gmail.com>
2024-08-24 17:49:25 +0800

    fix

--- emacs.d/lisp/init-python.el
+++ emacs.d/lisp/init-python.el
@@ -1,7 +1,20 @@
 (use-package python
-  :mode ("[/]BUILD\\'" . python-mode)
+  :mode (("[/]BUILD\\'" . python-mode)
+         ("\\.py\\'" . python-mode))
   :hook (python-mode . (lambda() (subword-mode 1) (setq python-indent-offset 4)))
   :bind (:map python-mode-map
               ("<return>" . newline-and-indent)))
+
+;; You should install the python package "black".
+(use-package python-black
+  :after python
+  :bind (:map python-mode-map
+              ("C-M-\\" . python-format)))
+
+(defun python-format ()
+  (interactive)
+  (if (use-region-p)
+      (python-black-region (region-beginning) (region-end))
+    (python-black-buffer)))
 
 (provide 'init-python)
