6619cc5da086c99242b4cddd53761a6e8112bef1
spockwang <spockwang@tencent.com>
2024-08-28 12:49:15 +0800

    fix

--- emacs.d/lisp/init-package.el
+++ emacs.d/lisp/init-package.el
@@ -1,23 +1,8 @@
 (require 'package)
-(setq package-archives '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
+(setq package-archives
+      '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+		("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
-
-(defun require-package (package &optional min-version no-refresh)
-  "Install given PACKAGE, optionally requiring MIN-VERSION.
-If NO-REFRESH is non-nil, the available package lists will not be
-re-downloaded in order to locate PACKAGE."
-  (if (package-installed-p package min-version)
-      t
-    (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package)
-      (progn
-        (package-refresh-contents)
-        (require-package package min-version t)))))
-
-;; (eval-when-compile
-;;   (require-package 'use-package)
-;;   (require 'use-package))
 
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
