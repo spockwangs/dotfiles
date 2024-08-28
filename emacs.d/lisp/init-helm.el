227850f468d13e236c3200fa0cce97e827390a62
spockwang <spockwang@tencent.com>
2024-08-28 17:34:03 +0800

    fix

---
+++ emacs.d/lisp/init-helm.el
@@ -0,0 +1,11 @@
+(use-package helm
+  :config
+  (require 'helm-autoloads)
+  (setq helm-buffers-fuzzy-matching t
+        helm-recentf-fuzzy-match t)
+  :bind (("M-x" . helm-M-x)
+         ("C-x C-f" . helm-find-files)
+         ("C-x b" . helm-mini)))
+
+
+(provide 'init-helm)
