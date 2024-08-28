4a7a663e58eb1d28ffe5281676db336c516c494b
spockwang <spockwang@tencent.com>
2024-08-28 13:31:50 +0800

    fix

--- emacs.d/lisp/util.el
+++ emacs.d/lisp/util.el
@@ -113,4 +113,16 @@ negative"
       (fill-region (region-beginning) (region-end))
     (fill-paragraph nil)))
 
+(defun util/delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (if filename
+        (if (y-or-n-p (format "Do you really want to delete file `%s'?" filename))
+            (progn
+              (delete-file filename)
+              (message "Deleted file `%s'." filename)
+              (kill-buffer)))
+      (message "No visited file!"))))
+
 (provide 'util)
