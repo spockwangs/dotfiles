d683fc1efd61cc3a380350feb86752a67cd1c11a
spockwang <wbbtiger@gmail.com>
2024-08-29 11:44:33 +0800

    Fixed sql-mode.

--- emacs.d/lisp/init-sql.el
+++ emacs.d/lisp/init-sql.el
@@ -5,8 +5,9 @@
   :config
   (require 'sqlformat)
   (setq sqlformat-command 'sqlformat)
-  (setq sqlformat-args '("-k" "upper" "-r" "--indent_width" "4" "-s"))
+  (setq sqlformat-args '("-k" "upper" "-r" "-s" "--strip-comments" "--indent_columns" "--encoding" "utf-8"))
   :hook (sql-mode . turn-on-auto-fill)
+  :mode (("\\.sql\\'" . sql-mode))
   :bind (:map sql-mode-map
               ("C-c C-c" . comment-region)
               ("C-M-\\" . sqlformat)))
