da44535b5e5e4e6017616a843bee97818e2f4e25
spockwang <wbbtiger@gmail.com>
2024-08-29 16:11:34 +0800

    Fixed use-package.

--- emacs.d/lisp/init-emacs-lisp.el
+++ emacs.d/lisp/init-emacs-lisp.el
@@ -1,8 +1,9 @@
 (use-package elisp-mode
   :ensure nil
-  :hook (emacs-lisp-mode . turn-on-auto-fill)
   :bind (:map emacs-lisp-mode-map
               ("C-c C-c" . comment-region)
-              ([(return)] . newline-and-indent)))
+              ([(return)] . newline-and-indent))
+  :config
+  (turn-on-auto-fill))
 
 (provide 'init-emacs-lisp)
--- emacs.d/lisp/init-sql.el
+++ emacs.d/lisp/init-sql.el
@@ -1,13 +1,12 @@
-(use-package reformatter)
-
-(use-package sql-mode
-  :ensure nil
+(use-package sqlformat
   :config
-  (require 'sqlformat)
   (setq sqlformat-command 'sqlformat)
-  (setq sqlformat-args '("-k" "upper" "-r" "-s" "--strip-comments" "--indent_columns" "--encoding" "utf-8"))
-  :hook (sql-mode . turn-on-auto-fill)
+  (setq sqlformat-args '("-k" "upper" "-r" "-s" "--strip-comments" "--indent_columns" "--encoding" "utf-8")))
+
+(use-package sql
   :mode (("\\.sql\\'" . sql-mode))
+  :config
+  (turn-on-auto-fill)
   :bind (:map sql-mode-map
               ("C-c C-c" . comment-region)
               ("C-M-\\" . sqlformat)))
