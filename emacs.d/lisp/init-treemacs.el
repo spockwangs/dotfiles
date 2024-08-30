22a3c8fefdc3b6b6ff12de75556f29456f384c68
spockwang <spockwang@tencent.com>
2024-08-30 20:17:14 +0800

    fix

--- emacs.d/lisp/init-treemacs.el
+++ emacs.d/lisp/init-treemacs.el
@@ -1,6 +1,6 @@
 (use-package treemacs
-  :config
-  (setq treemacs-width 70)
+  :custom
+  (treemacs-width (* 0.3 (frame-width)) "Set the treemacs window width to 30%")
   :bind (("<f8>" . treemacs)
          ("M-0" . treemacs-select-window)))
 
@@ -12,7 +12,6 @@
   ;; You should download (https://www.nerdfonts.com/font-downloads) and install the font "Symbols
   ;; Nerd Font" first.
   (require 'nerd-icons)
-  (treemacs-load-theme "nerd-icons")
-  (treemacs-toggle-fixed-width))
+  (treemacs-load-theme "nerd-icons"))
 
 (provide 'init-treemacs)
