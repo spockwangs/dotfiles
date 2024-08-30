f7b41f7627f3616fbafdc038118f7537f6959505
spockwang <spockwang@tencent.com>
2024-08-30 20:17:14 +0800

    fix

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,7 +2,7 @@
 ;; Copyright (c) 2010-2015 wbb
 ;;     All rights reserved.
 ;;
-;; Time-stamp: <2024-08-29 20:11:51 spockwang>
+;; Time-stamp: <2024-08-30 20:15:51 spockwang>
 ;;
 
 (require 'server)
