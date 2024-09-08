bfa11f8a9abac56749c5c461c803576cd17040f7
spockwang <wbbtiger@gmail.com>
2024-09-08 10:00:31 +0800

    fix

--- emacs.d/lisp/init-haskell.el
+++ emacs.d/lisp/init-haskell.el
@@ -1,11 +1,10 @@
 (use-package haskell-mode
   :mode ("\\.hsc?\\'" . haskell-mode)
+  :hook ((haskell-mode . turn-on-haskell-indent)
+         (haskell-mode . turn-on-haskell-doc-mode))
   :bind (:map haskell-mode-map
               ("C-c h" . haskell-hoogle)
               :map haskell-cabal-mode-map
-              ("C-c C-c" . comment-region))
-  :config
-  (turn-on-haskell-indent)
-  (turn-on-haskell-doc-mode))
+              ("C-c C-c" . comment-region)))
 
 (provide 'init-haskell)
