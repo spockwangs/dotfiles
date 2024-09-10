4be7162950e103bcb540a9c8d92a30e01eab18c9
spockwang <wbbtiger@gmail.com>
2024-09-10 20:05:57 +0800

    fix

--- emacs.d/lisp/init-helm.el
+++ emacs.d/lisp/init-helm.el
@@ -3,9 +3,11 @@
   (require 'helm-autoloads)
   (setq helm-buffers-fuzzy-matching t
         helm-recentf-fuzzy-match t)
+  (helm-autoresize-mode t)
   :bind (("M-x" . helm-M-x)
 ;         ("C-x C-f" . helm-find-files)
 ;         ("C-x b" . helm-mini))
+         ("C-c h g" . helm-google-suggest)
          ))
 
 (provide 'init-helm)
