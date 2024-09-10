142603cbfdfec25aeaefc0240f63b721f17dd0bd
spockwang <wbbtiger@gmail.com>
2024-09-10 10:46:01 +0800

    fix

--- install.bat
+++ install.bat
@@ -1,14 +1,23 @@
 rem Install scripts for Windows. It must be run as administrater.
 
-set DOTFILES_DIR=%~dp0%
+set DOTFILES_DIR=%~dp0
 
-for %%G in (emacs.d, bin) DO (
-    rmdir %HOME%\.%%G
-    mklink /D %HOME%\.%%G %DOTFILES_DIR%\%%G
+echo "DOTFILES_DIR=%DOTFILES_DIR%"
+mkdir "%HOME%\bin"
+for %%G in (bin\*) DO (
+    del /Q %HOME%\%%G
+    mklink %HOME%\%%G %DOTFILES_DIR%%%G
 )
 
+rem Link directories.
+for %%G in (emacs.d) DO (
+    rmdir /S /Q %HOME%\.%%G
+    mklink /D %HOME%\.%%G %DOTFILES_DIR%%%G
+)
+
+rem Link files.
 for %%G in (bash_profile, bashrc, dir_colors, fonts.conf, gitignore, indent.pro, vimrc,
             gitconfig, screenrc, inputrc) DO (
-    del %HOME%\.%%G
-    mklink %HOME%\.%%G %DOTFILES_DIR%\%%G
+    del /Q %HOME%\.%%G
+    mklink %HOME%\.%%G %DOTFILES_DIR%%%G
 )
