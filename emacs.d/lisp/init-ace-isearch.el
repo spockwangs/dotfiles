e295eb9208ffc3dc66a919965b7225cf20d714d4
spockwang <wbbtiger@gmail.com>
2024-10-17 20:44:48 +0800

    fix

--- emacs.d/lisp/init-ace-isearch.el
+++ emacs.d/lisp/init-ace-isearch.el
@@ -1,8 +1,12 @@
+(use-package avy)
+
 (use-package ace-isearch
   :hook
   (after-init . (lambda () (global-ace-isearch-mode 1)))
   :custom
-  (ace-isearch-jump-delay 1)
-  (ace-isearch-input-length 10 "Min length of input string to invoke helm search"))
+  (ace-isearch-function 'avy-goto-word-1)
+  (ace-isearch-input-length 100 "Min length of input string to invoke helm search")
+  :bind (:map isearch-mode-map
+              ("C-'" . avy-isearch)))
 
 (provide 'init-ace-isearch)
