ebaae13fbf0260e33589007d44d0eceaf3f136ce
spockwang <wbbtiger@gmail.com>
2024-10-28 12:29:19 +0800

    fix

--- emacs.d/lisp/init-tramp.el
+++ emacs.d/lisp/init-tramp.el
@@ -1,10 +1,14 @@
-;; Make method "ssh" of tramp mode work on windows.
-(when (eq system-type 'windows-nt)
-  (require 'cl-lib)
-  (with-eval-after-load 'tramp
+
+(require 'cl-lib)
+(with-eval-after-load 'tramp
+  ;; Make method "ssh" of tramp mode work on windows.
+  (when (eq system-type 'windows-nt)
     (cl-pushnew '("-tt")
                 (car (alist-get 'tramp-login-args
                                 (cdr (assoc "ssh" tramp-methods))))
-                :test #'equal)))
+                :test #'equal))
+  ;; Make the PATH setting in "~/.bashrc" available to the remote shell.
+  (add-to-list 'tramp-remote-path 'tramp-own-remote-path))
+
 
 (provide 'init-tramp)
