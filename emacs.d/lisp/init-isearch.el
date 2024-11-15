74d2a007c1f8b18516caaa23fd102a740695ca75
spockwang <wbbtiger@gmail.com>
2024-11-15 18:06:00 +0800

    fix

--- emacs.d/lisp/init-isearch.el
+++ emacs.d/lisp/init-isearch.el
@@ -6,6 +6,7 @@
   :hook
   (after-init . (lambda () (global-ace-isearch-mode 1)))
   :custom
+  (ace-isearch-jump-delay 0.5)
   (ace-isearch-function 'avy-goto-word-1)
   (ace-isearch-input-length 100 "Min length of input string to invoke helm search")
   :bind (:map isearch-mode-map
