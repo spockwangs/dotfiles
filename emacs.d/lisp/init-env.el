d4be1ea91dd70cfb97f09a5c4535ffaac4f00cc7
spockwang <wbbtiger@gmail.com>
2024-12-12 11:23:21 +0800

    fix

--- emacs.d/lisp/init-env.el
+++ emacs.d/lisp/init-env.el
@@ -31,12 +31,12 @@
   (util/add-exec-path "~/scoop/shims")
   (util/add-exec-path "~/scoop/apps/latex/24.1/texmfs/install/miktex/bin/x64"))
 
-(setq user-full-name "spockwang")
-(setq user-mail-address "wbbtiger@gmail.com")
+(setq user-full-name "spockwang"
+      user-mail-address "wbbtiger@gmail.com")
 (setenv "ORGANIZATION" "Tencent")
 
 ;; Set the icloud path.
-(defvar env/icloud-path
+(defconst env/icloud-path
   (cond ((eq system-type 'windows-nt) "~/iCloudDrive/")
         ((eq system-type 'darwin) "~/Library/Mobile Documents/com~apple~CloudDocs/"))
   "The iCloud path")
@@ -50,6 +50,27 @@
 (connection-local-set-profiles
  '(:application tramp :protocol "ssh" :machine "devcloud2")
  'remote-bash)
+
+(defun env/my-dpi (&optional frame)
+  "Get the DPI of FRAME (or current if nil)."
+  (cl-flet ((pyth (lambda (w h)
+                    (sqrt (+ (* w w)
+                             (* h h)))))
+            (mm2in (lambda (mm)
+                     (/ mm 25.4))))
+    (let* ((atts (frame-monitor-attributes frame))
+           (pix-w (cl-fourth (assoc 'geometry atts)))
+           (pix-h (cl-fifth (assoc 'geometry atts)))
+           (pix-d (pyth pix-w pix-h))
+           (mm-w (cl-second (assoc 'mm-size atts)))
+           (mm-h (cl-third (assoc 'mm-size atts)))
+           (mm-d (pyth mm-w mm-h)))
+      (/ pix-d (mm2in mm-d)))))
+
+(defconst env/font-size (round (* (env/my-dpi) 0.22))
+  "The default font size in pixels")
+(defconst env/font-size-for-chinese (round (* (env/my-dpi) 0.26))
+  "The font size in pixels for chinese")
 
 (provide 'init-env)
 ;;; init-env.el ends here
