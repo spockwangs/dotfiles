230da4eb569587989186aa115c689064c3609672
spockwang(王兵兵) <spockwang@tencent.com>
2025-08-28 17:20:07 +0800

    fix

---
+++ emacs.d/lisp/init-gptel.el
@@ -0,0 +1,35 @@
+;;; init-gptel.el --- Configure methods of accessing LLMs  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025  Tencent
+
+;; Author: spockwang <wbbtiger@gmail.com>
+;; Keywords: 
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+
+(use-package gptel
+  :config
+  (setq gptel-model 'gemini-2.5-flash
+        gptel-backend (gptel-make-gemini "Gemini"
+                        :key 'gptel-api-key
+                        :stream t)))
+
+(provide 'init-gptel)
+;;; init-gptel.el ends here
